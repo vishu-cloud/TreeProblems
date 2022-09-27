212b273db3bb83917ed46b53855a813f46619987
AnanyNayak <anany.nayak@in-sciencelabs.com>
2022-09-27 17:43:57 +0530

    prob11

--- prob11/src/IterativePreorder.java
+++ prob11/src/IterativePreorder.java
@@ -11,33 +11,20 @@ class Node
 }
 
 public class IterativePreorder {
-    public static int path(Node root, ArrayList<Node> arr, Node dest)
+    public static void path(Node root)
     {
-        if(root == dest)
-            return 1;
-        if(root.left!=null) {
-            arr.add(root.left);
-            int l = path(root.left, arr, dest);
-            if (l == 1)
-                return 1;
-            arr.remove(arr.size()-1);
+        Stack<Node> stk = new Stack<>();
+        stk.push(root);
+        while(!stk.isEmpty())
+        {
+            Node temp = stk.peek();
+            stk.pop();
+            System.out.print(temp.val+" ");
+            if(temp.right!=null)
+            stk.push(temp.right);
+            if(temp.left!=null)
+            stk.push(temp.left);
         }
-        if(root.right!=null) {
-            arr.add(root.right);
-            int r = path(root.right, arr, dest);
-            if (r == 1)
-                return 1;
-            arr.remove(arr.size()-1);
-        }
-        return 0;
-    }
-    public static void inorder(Node root)
-    {
-        if(root == null)
-            return;
-        inorder(root.left);
-        System.out.print(root.val + " ");
-        inorder(root.right);
     }
     public static void main(String args[])
     {
@@ -48,18 +35,7 @@ public class IterativePreorder {
         tree.left.right = new Node(5);
         tree.right.left = new Node(6);
         tree.right.right = new Node(7);
-        ArrayList<Node> arr = new ArrayList<>();
-        arr.add(tree);
-        int x = path(tree,arr,tree.left.left);
-        inorder(tree);
-        System.out.println();
-        for(int i=0;i<arr.size()/2;i++)
-        {
-            int temp = arr.get(i).val;
-            arr.get(i).val=arr.get(arr.size()-1-i).val;
-            arr.get(arr.size()-1-i).val = temp;
-        }
-        inorder(tree);
+        path(tree);
         //Level(tree);
     }
 }
